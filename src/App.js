ae950c4ed6115be56631fcae6a55d65f30b2cce7
Sanjay361899 <sanjaythakur361899@gmail.com>
2022-12-13 18:12:54 +0530

    checkout  preparing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Header from './Header';
-import './App.css'
+import Header from "./Header";
+import "./App.css";
+import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Checkout from "./Checkout";
 function App() {
   return (
-    <div className="App">
-   <Header/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/checkout" element={<Checkout />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/ServiceWorker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceWorker = () => {
+  return (
+    <div>ServiceWorker</div>
+  )
+}
+
+export default ServiceWorker
